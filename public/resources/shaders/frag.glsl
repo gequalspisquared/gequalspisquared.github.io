363890f828167df2b63bea70e75245f1f856b5a4
Nicholas Crane <nickcrusher30@gmail.com>
2025-05-09 23:45:40 -0600

    Mess around with shaders, create a CRTV effect with quincy

--- public/resources/shaders/frag.glsl
+++ public/resources/shaders/frag.glsl
@@ -1,3 +1,25 @@
+varying vec2 vUvs;
+
+uniform float time;
+uniform sampler2D diffuse;
+
+float inverseLerp(float v, float minValue, float maxValue) {
+    return (v - minValue) / (maxValue - minValue);
+}
+
+float remap(float v, float inMin, float inMax, float outMin, float outMax) {
+    float t = inverseLerp(v, inMin, inMax);
+    return mix(outMin, outMax, t);
+}
+
 void main() {
-    gl_FragColor = vec4(0.1, 0.2, 0.7, 1.0);
+    vec3 color = vec3(0.0);
+
+    float t = remap(sin(vUvs.y * 200.0 + time), -1.0, 1.0, 0.5, 0.75);
+
+    vec4 diffuseSample = texture2D(diffuse, vUvs);
+    color = mix(vec3(t), diffuseSample.xyz, 0.8);
+    // color = vec3(t);
+
+    gl_FragColor = vec4(color, 1.0);
 }
--- src/app.js
+++ src/app.js
@@ -31,28 +31,57 @@ class App {
     const vert_shader = await fetch("./resources/shaders/vert.glsl");
     const frag_shader = await fetch("./resources/shaders/frag.glsl");
 
+    const loader = new THREE.TextureLoader();
+    const quincyTexture = loader.load("./resources/images/quincy.jpg");
+    // const overlayTexture = loader.load("./resources/images/overlay.png");
+
     const material = new THREE.ShaderMaterial({
-      uniforms: {},
+      uniforms: {
+        diffuse: { value: quincyTexture },
+        resolution: {
+          value: new THREE.Vector2(window.innerWidth, window.innerHeight),
+        },
+        time: { value: 0.0 },
+      },
       vertexShader: await vert_shader.text(),
       fragmentShader: await frag_shader.text(),
     });
+    this.material_ = material;
 
     const geometry = new THREE.PlaneGeometry(1, 1);
 
     const plane = new THREE.Mesh(geometry, material);
     plane.position.set(0.5, 0.5, 0);
     this.scene_.add(plane);
+
+    this.totalTime_ = 0.0;
   }
 
   onWindowResize_() {
     this.threejs_.setSize(window.innerWidth, window.innerHeight);
+    this.material_.uniforms.resolution.value = new THREE.Vector2(
+      window.innerWidth,
+      window.innerHeight
+    );
   }
 
   raf_() {
     requestAnimationFrame((t) => {
+      if (this.previousRAF_ === null) {
+        this.previousRAF_ = t;
+      }
+
+      this.step_(t - this.previousRAF_);
       this.threejs_.render(this.scene_, this.camera_);
       this.raf_();
+      this.previousRAF_ = t;
     });
+  }
+
+  step_(timeElapsed) {
+    const timeElapsedS = timeElapsed * 0.001;
+    this.totalTime_ += timeElapsedS;
+    this.material_.uniforms.time.value = this.totalTime_;
   }
 }
 
